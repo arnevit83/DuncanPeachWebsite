016ca0f532589e7224cc9aa481f06079952979ae
duncan <arnevit@msn.com>
2019-11-04 16:24:24 +0000

    Removed LucyE Bar the logos

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'LucyEdgeApp'`, () => {
+  it(`should have as title 'DuncanPeachApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('LucyEdgeApp');
+    expect(app.title).toEqual('DuncanPeachApp');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to LucyEdgeApp!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to DuncanPeachApp!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,27 +12,11 @@ import { PageNotFoundComponent } from './PageNotFound/app.PageNotFound';
 import { HomeComponent } from './ContentArea/app.Home';
 import { SkillsComponent } from './ContentArea/app.Skills';
 import { SuccessesComponent } from './ContentArea/app.Successes';
-import { ClientsComponent } from './ContentArea/app.Clients';
-import { YogaTeachersComponent } from './ContentArea/app.YogaTeachers';
 import { TestimonialsComponent } from './ContentArea/app.Testimonials';
 import { LovesComponent } from './ContentArea/app.Loves';
 import { ContactComponent } from './ContentArea/app.Contact';
-import { BrandStrategyComponent } from './ContentArea/app.BrandStrategy';
-import { CopywriteringComponent } from './ContentArea/app.Copywritering';
 import { InspoComponent } from './ContentArea/app.Inspo';
 
-import { BrandpurposeComponent } from './ContentArea/Successes/app.Brandpurpose';
-import { BrandpositioningComponent } from './ContentArea/Successes/app.Brandpositioning';
-import { BrandstrategytrainingcoursesComponent } from './ContentArea/Successes/app.Brandstrategytrainingcourses';
-import { CustomerinsightComponent } from './ContentArea/Successes/app.Customerinsight';
-import { CopywritingresearchconceptsComponent } from './ContentArea/Successes/app.Copywritingresearchconcepts';
-import { WebsitecopywriterComponent } from './ContentArea/Successes/app.Websitecopywriter';
-import { SEOCopywritingComponent } from './ContentArea/Successes/app.SEOCopywriting';
-import { ToneofvoiceComponent } from './ContentArea/Successes/app.Toneofvoice';
-import { ContentmarketingComponent } from './ContentArea/Successes/app.Contentmarketing';
-import { YogaschooldropoutComponent } from './ContentArea/Successes/app.Yogaschooldropout';
-import { ThehandbagwelliesyogaclubComponent } from './ContentArea/Successes/app.Thehandbagwelliesyogaclub';
-import { DowndogbillionaireComponent } from './ContentArea/Successes/app.Downdogbillionaire';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
@@ -48,35 +32,21 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HomeComponent,
     SkillsComponent,
     SuccessesComponent,
-    ClientsComponent,
-    YogaTeachersComponent,
     TestimonialsComponent,
     LovesComponent,
     ContactComponent,
     NavComponent,
-    BrandStrategyComponent,
-    CopywriteringComponent,
     InspoComponent,
-    PageNotFoundComponent,
-    BrandpurposeComponent,
-    BrandpositioningComponent,
-    BrandstrategytrainingcoursesComponent,
-    CustomerinsightComponent,
-    CopywritingresearchconceptsComponent,
-    WebsitecopywriterComponent,
-    SEOCopywritingComponent,
-    ToneofvoiceComponent,
-    ContentmarketingComponent,
-    YogaschooldropoutComponent,
-    ThehandbagwelliesyogaclubComponent,
-    DowndogbillionaireComponent
+    PageNotFoundComponent
+
 
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    FontAwesomeModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    FontAwesomeModule
+    // ,
+    // ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [Title],
   bootstrap: [AppComponent]
