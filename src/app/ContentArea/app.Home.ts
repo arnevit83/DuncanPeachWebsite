6622aa5fecb2223a1a9e4676689214fabd9c28b5
duncan <arnevit@msn.com>
2019-11-06 12:17:04 +0000

    Homepage

--- src/app/ContentArea/app.Home.ts
+++ src/app/ContentArea/app.Home.ts
@@ -2,19 +2,20 @@ import { Component } from '@angular/core';
 import { Title }     from '@angular/platform-browser';
 import { Meta }     from '@angular/platform-browser';
 
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-
+// import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+// import { faPhone } from '@fortawesome/free-solid-svg-icons';
+// import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'Home',
   templateUrl: './Home.html',
 })
 export class HomeComponent {
-    faChevronDown = faChevronDown;
-    faPhone = faPhone;
-    faEnvelope = faEnvelope;
+    // faChevronDown = faChevronDown;
+    // faPhone = faPhone;
+    // faEnvelope = faEnvelope;
+    faAngleDoubleDown = faAngleDoubleDown;
     public constructor(private titleService: Title, private meta: Meta ) {
         //  this.titleService.setTitle( "" );
         //  this.meta.updateTag({ name: 'description', content: '' });
