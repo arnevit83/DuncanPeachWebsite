99b9885e2f0c4aa3d75eb14a957c720deb7cbc68
Duncan Peach <other@duncanpeach.com>
2022-06-15 17:00:42 +0100

    Web3

--- src/app/ContentArea/app.Skills.ts
+++ src/app/ContentArea/app.Skills.ts
@@ -1,34 +1,25 @@
-import { Component } from '@angular/core';
-import { Title }     from '@angular/platform-browser';
-import { Meta }     from '@angular/platform-browser';
-//import { faChartLine } from '@fortawesome/free-solid-svg-icons';
-//import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { Component } from "@angular/core";
+import { Title } from "@angular/platform-browser";
+import { Meta } from "@angular/platform-browser";
+
 declare var $: any;
 
 @Component({
-  selector: 'Skills',
-  templateUrl: './Skills.html',
+  selector: "Skills",
+  templateUrl: "./Skills.html",
 })
-
-
 export class SkillsComponent {
- // fachartline = faChartLine;
- // faPencilAlt = faPencilAlt;
-
-
   public constructor(private titleService: Title, private meta: Meta) {
-          //  this.titleService.setTitle( "" );
-        //  this.meta.updateTag({ name: 'description', content: '' });
-        //  this.meta.updateTag({ name: 'keywords', content: '' });  }
-      }
-      ngOnInit() {
-        $(function() {
-         
-       
-          setTimeout(function() {
-            var spanWidth = $('.text span').width();
-            $('.text').animate( { width: spanWidth }, 1000 );
-        }, 100);
-        });
-      };
+    //  this.titleService.setTitle( "" );
+    //  this.meta.updateTag({ name: 'description', content: '' });
+    //  this.meta.updateTag({ name: 'keywords', content: '' });  }
+  }
+  ngOnInit() {
+    $(function () {
+      setTimeout(function () {
+        var spanWidth = $(".text span").width();
+        $(".text").animate({ width: spanWidth }, 1000);
+      }, 100);
+    });
+  }
 }
